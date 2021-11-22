52ff55ed46328f36d36ec208c4e90fb5409e8d21
thodec0 <94637516+thodec0@users.noreply.github.com>
2021-11-22 13:58:40 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dever
+# project_c9_pro_new
---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("red");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("purple");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("brown");
+  }
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    background("green");
+  }
+
+  
+  drawSprites();
+}
+
